e4b7099fa4275910cdd15119c8d3c03a8307a0a9
akshdeep996 <rajawatakshdeep@gmail.com>
2017-10-01 09:06:06 +0530

    Added recipe and shopping

--- src/app/root/recipi/recipi-list/recipi-list.component.ts
+++ src/app/root/recipi/recipi-list/recipi-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Recipe} from '../recipi.model';
+
 @Component({
   selector: 'app-recipi-list',
   templateUrl: './recipi-list.component.html',
@@ -7,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipiListComponent implements OnInit {
 
+recipes: Recipe[] = [
+
+new Recipe('A test Recipe','This is simply to test','http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
+new Recipe('A second Recipe','This is simply to test','http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg')
+
+
+];
   constructor() { }
 
   ngOnInit() {
--- src/app/root/shopping/shopping.component.ts
+++ src/app/root/shopping/shopping.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core'
 
+import {Ingredient} from '../shared/ingredient.model';
+
 @Component({
 
 selector:'app-shop',
@@ -8,5 +10,12 @@ styleUrls:['shopping.component.css']
 })
 
 export class ShoppingComponent {
+
+    ingredients: Ingredient[] = [
+
+        new Ingredient('Apples',5),
+        new Ingredient('Tomatoes',4),
+        
+    ];
     
 }
