bc39a14165567e320cf05ca27aaceaa56015aee1
akshdeep996 <rajawatakshdeep@gmail.com>
2017-10-05 20:44:50 +0530

    deleted unwanted files

--- src/app/root/recipi/recipi-detail/recipi-detail.component.ts
+++ src/app/root/recipi/recipi-detail/recipi-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import {Recipe} from '../recipi.model';
+
 
 @Component({
   selector: 'app-recipi-detail',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipi-detail.component.css']
 })
 export class RecipiDetailComponent implements OnInit {
+
+@Input() recipe: Recipe; 
 
   constructor() { }
 
